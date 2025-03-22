171bde27298f65393f9aa0e31ce0d330b2a15bed
Rayomizador <rayosanchez1@gmail.com>
2025-03-22 22:23:35 +0000

    avance1

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
-let nuevoDiv = document.createElement('div');
-nuevoDiv.innerHTML = '<p>Este es un nuevo div</p>';
-document.body.appendChild(nuevoDiv);
-
-let lista = document.getElementById('lista');
-let nuevoItem = document.createElement('li');
-nuevoItem.textContent = 'Nuevo ítem';
-lista.appendChild(nuevoItem);
-
-let primerItem = lista.firstChild;
-lista.insertBefore(nuevoItem, primerItem);
+document.getElementById('cierreCajaForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+  
+    const VEfectivo = parseFloat(document.getElementById('Vefectivo').value);
+    const VTarjeta = parseFloat(document.getElementById('Vtarjeta').value);
+    const VentaTotal = VEfectivo + VTarjeta;
+  
+    const resultadosTable = document.getElementById('resultadosTable').getElementsByTagName('tbody')[0];
+    resultadosTable.innerHTML = `
+      <tr>
+        <td>Vefectivo</td>
+        <td>${VEfectivo}</td>
+      </tr>
+      <tr>
+        <td>Vtarjeta</td>
+        <td>${VTarjeta}</td>
+      </tr>
+      <tr>
+        <td>Vtotal</td>
+        <td>${VentaTotal}</td>
+      </tr>
+    `;
+  });
